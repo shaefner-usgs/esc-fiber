d51e6bae7c0572d1f503983362422d8da9c48dce
Scott Haefner <shaefner@usgs.gov>
2024-09-03 10:30:33 -0700

    update model: indexed array, lowercase keys, add count

--- src/htdocs/json/metadata.json.php
+++ src/htdocs/json/metadata.json.php
@@ -16,6 +16,7 @@ $protocol = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) === 'on' ?
 $template = [
   'id' => $cable,
   'metadata' => [
+    'count' => 0,
     'generated' => floor(microtime(true) * 1000),
     'url' => "$protocol://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']
   ],
@@ -27,13 +28,13 @@ $refs = [];
 
 // Store references in an Array.
 while ($ref = $rsReferences->fetch(PDO::FETCH_OBJ)) {
-  $key = 'experiment' . $ref->experiment_id;
+  $id = 'experiment' . $ref->experiment_id;
 
-  if (!array_key_exists($key, $refs)) {
-    $refs[$key] = [];
+  if (!array_key_exists($id, $refs)) {
+    $refs[$id] = [];
   }
 
-  $refs[$key][] = [
+  $refs[$id][] = [
     'author' => $ref->author,
     'doi' => $ref->doi,
     'title' => $ref->title,
@@ -43,7 +44,7 @@ while ($ref = $rsReferences->fetch(PDO::FETCH_OBJ)) {
 
 // Store experiments in an Array (add add the references).
 while ($exp = $rsMetadata->fetch(PDO::FETCH_OBJ)) {
-  $key = 'experiment' . $exp->experiment_id;
+  $id = 'experiment' . $exp->experiment_id;
   $path = $APP_DIR . '/htdocs/img/plots';
   $plot = $exp->cable_id . '-' . $exp->experiment_id . '.png';
   $references = []; // default
@@ -51,12 +52,13 @@ while ($exp = $rsMetadata->fetch(PDO::FETCH_OBJ)) {
   if (!file_exists("$path/$plot")) {
     $plot = '';
   }
-  if (array_key_exists($key, $refs)) {
-    $references = $refs[$key];
+  if (array_key_exists($id, $refs)) {
+    $references = $refs[$id];
   }
 
-  $experiments[$key] = [
-    'Acquisition' => [
+  $experiments[] = [
+    'id' => $id,
+    'acquisition' => [
       'channels' => $exp->channels,
       'endtime' => $exp->end_time,
       'interval' => $exp->spatial_interval,
@@ -64,11 +66,11 @@ while ($exp = $rsMetadata->fetch(PDO::FETCH_OBJ)) {
       'rate' => $exp->sample_rate,
       'starttime' => $exp->start_time
     ],
-    'Interrogator' => [
+    'interrogator' => [
       'manufacturer' => $exp->manufacturer,
       'model' => $exp->model
     ],
-    'Overview' => [
+    'overview' => [
       'doi' => $exp->doi,
       'enddate' => $exp->end_date,
       'pi' => $exp->pi,
@@ -77,12 +79,13 @@ while ($exp = $rsMetadata->fetch(PDO::FETCH_OBJ)) {
       'reference' => $exp->reference,
       'startdate' => $exp->start_date
     ],
-    'References' => $references
+    'references' => $references
   ];
 }
 
 // Render the JSON feed
 $template['experiments'] = $experiments;
+$template['metadata']['count'] = count($experiments);
 
 setHeaders();
 
